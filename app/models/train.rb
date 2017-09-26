dcea507795d5bc1db26ccb40ede1ce1f24e3b372
AnatolySt <native.today@gmail.com>
2017-09-26 19:01:40 +0300

    Add Carriages v2

--- app/models/train.rb
+++ app/models/train.rb
@@ -4,4 +4,28 @@ class Train < ApplicationRecord
 
   has_many :tickets
   has_many :carriages
+
+  def count_carriages(kind)
+    @carriages_count = 0
+    self.carriages.each do |carriage|
+      if carriage.kind == kind
+        @carriages_count += 1
+      end
+    end
+    return @carriages_count
+  end
+
+  def count_seats(kind, seats_type)
+    @seats_count = 0
+    self.carriages.each do |carriage|
+      if carriage.kind == kind
+        if seats_type == 'bottom_seats'
+          @seats_count += carriage.bottom_seats
+        elsif seats_type == 'top_seats'
+          @seats_count += carriage.top_seats
+        end
+      end
+    end
+    return @seats_count
+  end
 end
