b7dcf8f0ab57e7a3f3d290dfda2e29f426319245
AnatolySt <native.today@gmail.com>
2017-09-28 11:31:27 +0300

    Lesson 15 v3

--- app/models/train.rb
+++ app/models/train.rb
@@ -6,26 +6,26 @@ class Train < ApplicationRecord
   has_many :carriages
 
   def count_carriages(kind)
-    @carriages_count = 0
+    carriages_count = 0
     self.carriages.each do |carriage|
       if carriage.kind == kind
-        @carriages_count += 1
+        carriages_count += 1
       end
     end
-    return @carriages_count
+    return carriages_count
   end
 
   def count_seats(kind, seats_type)
-    @seats_count = 0
+    seats_count = 0
     self.carriages.each do |carriage|
       if carriage.kind == kind
         if seats_type == 'bottom_seats'
-          @seats_count += carriage.bottom_seats
+          seats_count += carriage.bottom_seats
         elsif seats_type == 'top_seats'
-          @seats_count += carriage.top_seats
+          seats_count += carriage.top_seats
         end
       end
     end
-    return @seats_count
+    return seats_count
   end
 end
