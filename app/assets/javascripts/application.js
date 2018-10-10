eb7238db0a9b4da7c99a7b9193dcdd28ea8c4242
AnatolySt <an@webbery.pro>
2018-10-10 19:30:00 +0300

    Lesson 21 JavaScript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require bootstrap.min
